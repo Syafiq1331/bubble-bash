1d4fa97cdb714f321eca628bd4b85b41b68bd71d
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-05-01 22:12:37 +0700

    add:our services component

---
+++ src/components/OurServices.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+import Icon from '../assets/icons/FigmaLogo.svg'
+
+interface OurServicesProps { }
+
+const OurServices: React.FC<OurServicesProps> = ({ }) => {
+  return (
+    <section className=' bg-[#F9F9F9] py-[60px]'>
+      <div className='container mx-auto'>
+        <p className='text-[18px] text-[#2A2C3D]/70'>Our Services</p>
+        <h3 className='text-[36px] font-[600]'>We Provide What You Need</h3>
+
+        <div className='grid gap-x-6 grid-cols-3 gap-y-12 px-12 my-14'>
+          <Card image={Icon} title='UI/UX Designer' desc='We are ready to make your website more friendly and efficient in the eyes of users' />
+          <Card image={Icon} title='UI/UX Designer' desc='We are ready to make your website more friendly and efficient in the eyes of users' />
+          <Card image={Icon} title='UI/UX Designer' desc='We are ready to make your website more friendly and efficient in the eyes of users' />
+          <Card image={Icon} title='UI/UX Designer' desc='We are ready to make your website more friendly and efficient in the eyes of users' />
+          <Card image={Icon} title='UI/UX Designer' desc='We are ready to make your website more friendly and efficient in the eyes of users' />
+          <Card image={Icon} title='UI/UX Designer' desc='We are ready to make your website more friendly and efficient in the eyes of users' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default OurServices;
