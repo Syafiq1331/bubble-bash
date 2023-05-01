267eb91d0bd70abc60508f664d65944f25f98673
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-05-01 22:10:10 +0700

    add:new card portfolio

---
+++ src/components/Card__Portfolio.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface CardProps {
+  title?: string;
+  desc?: string;
+  image?: string;
+}
+
+const Card: React.FC<CardProps> = ({ title, desc, image }) => {
+  return (
+    <div className='mx-auto py-[32px] p-[60px] '>
+      <img src={image} className='mx-auto mb-[16px]' alt="" />
+      <h5 className='font-semibold text-[24px] mb-[6px] text-[#2A2C3D]'>{title}</h5>
+      <p className='text-[16px] text-[#2A2C3D]/70 font-[400]'>{desc}</p>
+    </div>
+  );
+};
+
+export default Card;
